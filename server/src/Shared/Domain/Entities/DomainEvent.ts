03b2da37b3be10bb157eae13c2886a217c41d906
laubernal <lbernalrodrguez@gmail.com>
2024-03-24 12:12:03 +0100

    add nest schema

--- server/src/Shared/Domain/Entities/DomainEvent.ts
+++ server/src/Shared/Domain/Entities/DomainEvent.ts
@@ -1,20 +1,19 @@
-import { IEvent } from "@nestjs/cqrs";
-import { DateVo } from "Shared/Domain/Vo/Date.vo";
-import { ID } from "../Vo/Id.vo";
+import { IEvent } from '@nestjs/cqrs';
+import { ID } from '../Vo/Id.vo';
 
 export abstract class DomainEvent implements IEvent {
   //<company>.<bounded-context>.<version>.<message-type>.<domain-entity>.<action>
   protected abstract readonly _type: string;
   protected abstract readonly _aggregateId: ID;
 
-  protected readonly _dateOccurred = DateVo.now();
+  protected readonly _dateOccurred = new Date();
   protected readonly _id: ID = ID.generate();
 
   public get id(): ID {
     return this._id;
   }
 
-  public get dateOccurred(): DateVo {
+  public get dateOccurred(): Date {
     return this._dateOccurred;
   }
 }
